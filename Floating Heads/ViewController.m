b467bb4c4a41b5c86417c83b4a47759e694be787
Chelsea Liu <cl314@duke.edu>
2015-07-15 12:18:42 -0400

    Initial Commit

---
+++ Floating Heads/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Floating Heads
+//
+//  Created by Chelsea Liu on 7/15/15.
+//  Copyright (c) 2015 Chelsea Liu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
